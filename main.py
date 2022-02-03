64234690157ed6e6bc90afe547c431ca21fd431b
fathan <fathan.a.dev@gmail.com>
2022-02-03 20:52:12 +0700

    encrypt/decrypt every file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
 import math
+import os
 
 # use icon.png for a test
 def encrypt(filename):
     file = open(filename, "rb")
     byteArray = bytearray(file.read())
+    print(len(byteArray))
     
     f = lambda x : (x + len(byteArray) - 2)%256
     
@@ -16,11 +18,12 @@ def encrypt(filename):
     writefile.write(byteArray)
     writefile.close()
 
-    return "successfully encrypted a file"
+    return "successfully encrypted a file : " + filename
 
 def decrypt(filename):
     file = open(filename, "rb")
     byteArray = bytearray(file.read())
+    print(len(byteArray))
     f = lambda x : (x - len(byteArray) + 2)%256
     writefile = open(filename, "wb")
     for i,v in enumerate(byteArray):
@@ -32,17 +35,48 @@ def decrypt(filename):
     writefile.write(byteArray)
     writefile.close()
 
-    return "successfully decrypted a file"
+    return "successfully decrypted a file : " + filename
+
+def encryptAllFile():
+    a = next(os.walk('.'))
+    r = []
+    if len(a) > 2:
+        b = a[2]
+    else:
+        b = a[1]
+    for i in b:
+        if i != "main.py":
+            r.append(encrypt(i))
+    return r
+
+def decryptAllFile():
+    a = next(os.walk('.'))
+    r = []
+    if len(a) > 2:
+        b = a[2]
+    else:
+        b = a[1]
+    for i in b:
+        if i != "main.py":
+            r.append(decrypt(i))
+    return r
+
 
 if __name__ == "__main__":
     while True:
-        filename = input("filename: ")
-        option = input("(1. encrypt | 2. decrypt) : ")
+        
+        option = input("(1. encrypt | 2. decrypt | 3. encrypt all files (current script folder) | 4. decrypt all files (current script folder)) : ")
         if option == "1":
-            
+            filename = input("filename: ")
             print(encrypt(filename))
         elif option == "2":
-            
+            filename = input("filename: ")
             print(decrypt(filename))
+        elif option == "3":
+            for i in encryptAllFile():
+                print(i)
+        elif option == "4":
+            for i in decryptAllFile():
+                print(i)
         else:
-            print("please choose 1 or 2")
+            print("please choose a number")
